f0764f9da445a53585dd401cb860f1b07cd95d27
NandiniGupta9 <nandinigupta8354@gmail.com>
2026-02-05 10:22:02 +0530

    Work progress in ExtraWork

--- ES/server.js
+++ ES/server.js
@@ -1,19 +1,49 @@
 import express from 'express';
 const app=express();
-app.set("view engine","ejs")
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended:true}))
 app.get("/",(req,res)=>{
     res.render("index")
-})
+});
+
+
+ let userData=[
+        {id:1,name:"nandini",age: "20",},
+        {id:2,name:"mahak",age: "21",},
+        {id:3,name:"teesha",age: "21",},
+ 
+];
 app.get("/user",(req,res)=>{
-    let userData={
-        name:"nandini",
-        age: "20",
-    }
-    res.render("user",{userData});0
+   
+    res.render("user",{userData});
 })
 app.get("/list",(req,res)=>{
     let arr=["apple","mango","strawberry"]
     res.render("list",{arr})
+})
+app.post("/api/user",(req,res)=>{
+    const {name,age} =req.body;
+    let newUserData ={
+        id: userData.length+1,
+        name,
+        age
+    }
+    userData.push(newUserData);
+    res.redirect('/user');
+    
+})
+app.get("/api/user/:id",(req,res)=>{
+    const userid =req.params.id;
+    const useridx =userData.findIndex((ele)=>ele.id==userid);
+    if(userid== -1)
+{
+    return res.send("/userNotFound")
+}
+userData.splice(useridx,1);
+res.redirect("/user")
+});
+app.get("/userNotFound",(req,res)=> {
+    res.render("userNotFound")
 })
 app.listen(3000,()=>{
     console.log("server is running")
