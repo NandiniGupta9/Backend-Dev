b3fb904a91090efa8c6a4ce1c773394132e46de7
NandiniGupta9 <nandinigupta8354@gmail.com>
2026-03-23 09:52:36 +0530

    Added database exercises with proper structure

---
+++ MongoDB-CRUD/database-exercises/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
