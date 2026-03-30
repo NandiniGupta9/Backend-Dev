80ef4cb0c2962dedb00eeebf4977e31346114f5c
NandiniGupta9 <nandinigupta8354@gmail.com>
2026-03-30 14:58:12 +0530

    code edited

---
+++ MongoDbCrud/demo.js
@@ -0,0 +1,56 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+
+const app=express();
+
+app.use(cookieParser('my-super-secret-key'));
+
+app.get('/login',(req,res)=>{
+
+    let user={
+        name:"rohan",
+        email:"rohan@example.com"
+    };
+    const token =jwt.sign(user,"qwertyuiop",{expiresIn:"1h"});
+    console.log(token);
+
+    res.cookie("token",token,{httpOnly:true});
+    res.send('Cookie has been set!');
+});
+const authMiddleware=(req,res,next)=>{
+    if(!req.cookies.token){
+        res.send("Invalid user");
+        return;
+    }
+    const token=req.cookies.token;
+    const decode=jwt.verify(token,"qwertyuiop");
+
+    req.decode;
+
+        next();
+}
+app.get('/dashboard', authMiddleware,(req,res)=>{
+    const decode=req.decode;
+    console.log(decode);
+    res.send(`Cookie value: ${decode.name}`);
+});
+app.get('/get-cookie', authMiddleware,(req,res)=>{
+    const decode=req.decode;
+    console.log(decode);
+    res.send(`Cookie value: ${decode.name}`);
+});
+
+
+app.get('/profile',authMiddleware,(req,res)=>{
+    const decode = req.decode;
+    console.log(decode);
+    res.send(`Welcome to your profile, ${decode.name}!`);
+});
+
+app.get('/logout',authMiddleware,(req,res)=>{
+    res.clearCookie('token');
+    res.send('You have been logged out');
+})
+
+app.listen(3000,()=>console.log("Server is running on port 3000"));
