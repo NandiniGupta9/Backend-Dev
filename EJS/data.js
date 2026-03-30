8785f6e0320c20c2db2876dbff809127f682e8a9
NandiniGupta9 <nandinigupta8354@gmail.com>
2026-03-30 15:19:43 +0530

    added

---
+++ EJS/data.js
@@ -0,0 +1,17 @@
+export let userData = [
+  {
+    id:1,
+    name: "Amit",
+    age: 23,
+  },
+  {
+    id:2,
+    name: "Rahul",
+    age: 25,
+  },
+  {    
+    id:3,
+    name: "Rohit",
+    age: 22,
+  },
+];
